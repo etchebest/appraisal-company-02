2ee075b033a3e5d8b4673f86c084c16051cffd80
cristian.etchebest <cristian.etchebest@meta.com.br>
2022-01-14 14:49:59 -0300

    Criando toTop

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,5 @@
 const btnMobile = document.getElementById("btn-mobile");
+const backTotop = document.getElementById("to-top");
 
 function toggleMenu(event) {
   if (event.type === "touchstart") event.preventDefault();
@@ -14,5 +15,28 @@ function toggleMenu(event) {
     event.currentTarget.setAttribute("aria-label", "Abrir Menu");
   }
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function topFunction() {
+  document.body.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+  document.documentElement.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+}
+function scrollFunction(){
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    backTotop.style.display = "block";
+  } else {
+    backTotop.style.display = "none";
+  }
+}
+
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
+
+backTotop.addEventListener("click", topFunction);
